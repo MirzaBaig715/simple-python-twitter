7a6428d72ccc69d3c5672896643358cee30e2798
Mirza Musharaf Baig <musharaf715@gmail.com>
2018-07-01 02:35:35 +0500

    update settings

--- simple-twitter-project/settings.py
+++ simple-twitter-project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Challenge_task.urls'
+ROOT_URLCONF = 'simple-twitter-project.urls'
 
 TEMPLATES = [
     {
